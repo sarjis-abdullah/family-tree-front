11e088d39edfa47f379922ddf83f784641405c1c
Sarjis Abdullah <55045212+sarjis-abdullah@users.noreply.github.com>
2025-05-07 11:36:02 +0600

    completed data addition

--- components/AddNewFamilyMember.vue
+++ components/AddNewFamilyMember.vue
@@ -2,26 +2,27 @@
     <v-row justify="center">
         <v-col cols="12" md="3">
 
-
-            <v-form ref="form">
+            <v-toolbar density="compact" title="Add new member" class="mb-2"></v-toolbar>
+            <v-form ref="form" @submit.prevent="validate()" lazy-validation>
                 <v-text-field v-model="name" :counter="10" :rules="nameRules" label="Name" required></v-text-field>
 
                 <v-select v-model="gender" :items="genders" :rules="[v => !!v || 'Gender selection is required']"
                     label="Gender" required></v-select>
 
-                <v-date-input prepend-icon="" label="Birth date"></v-date-input>
-                <v-autocomplete v-model="fatherId" :rules="fatherRules" label="Father Name"
-                    :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"></v-autocomplete>
-                <v-autocomplete v-model="motherId" :rules="motherRules" label="Mother Name"
-                    :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"></v-autocomplete>
+                <v-date-input :rules="dateRules" prepend-icon="" v-model="birthDate" label="Birth date"></v-date-input>
+                <v-autocomplete v-model="fatherId" :rules="fatherRules" label="Father Name" item-title="name"
+                    item-value="id" :items="usersData">
+                </v-autocomplete>
+                <v-autocomplete v-model="motherId" :rules="motherRules" label="Mother Name" item-title="name"
+                    item-value="id" :items="usersData">
+                </v-autocomplete>
 
-                <div class="d-flex flex-column">
-                    <v-btn class="mt-4" color="success" block @click="validate">
-                        Save member
-                    </v-btn>
-
-                    <v-btn class="mt-4" color="error" block @click="reset">
+                <div class="d-flex justify-end ga-2 mt-6">
+                    <v-btn class="" color="error" @click="reset">
                         Reset data
+                    </v-btn>
+                    <v-btn class="" color="success" type="submit" :loading="loading">
+                        Save member
                     </v-btn>
                 </div>
             </v-form>
@@ -30,17 +31,35 @@
 </template>
 <script setup>
 import { ref } from 'vue'
+import { UserService } from '~/services/UserService'
 
-const form = ref()
+const form = ref(null)
 
 const genders = [
     'Male',
     'Female',
 ]
 
-const name = ref('')
-const motherId = ref('')
-const fatherId = ref('')
+const name = ref('Test name')
+const usersData = ref([
+    {
+        id: 1,
+        name: "Abdur Rahman",
+        gender: "M",
+        mo: "Abdur Rahman",
+        fa: "Abdur Rahman",
+    },
+    {
+        id: 2,
+        name: "Fatima Rahman",
+        gender: "F",
+        mo: "Fatima Rahman",
+        fa: "Fatima Rahman",
+    }
+])
+const motherId = ref(2)
+const fatherId = ref(1)
+const birthDate = ref(new Date().toISOString().substring(0, 10))
 const nameRules = ref([
     v => !!v || 'Name is required',
     v => (v && v.length >= 3) || 'Name must be 3 characters atleast',
@@ -50,19 +69,46 @@ const dateRules = ref([
 ])
 const motherRules = ref([
     v => !!v || 'Mother name is required',
-    v => (v && v.length >= 3) || 'Mother name must be 3 characters atleast',
 ])
 const fatherRules = ref([
     v => !!v || 'Father name is required',
-    v => (v && v.length >= 3) || 'Father name must be 3 characters atleast',
 ])
-const gender = ref(null)
-const checkbox = ref(false)
+const gender = ref('Male')
+const loading = ref(false)
 
-async function validate() {
+const userData = computed(() => {
+    return {
+        name: name.value,
+        mother_id: motherId.value,
+        father_id: fatherId.value,
+        gender: gender.value.toLocaleLowerCase(),
+        birth_date: birthDate.value,
+    }
+})
+
+onMounted(() => {
+    // reset()
+    // getAllUsers()
+})
+
+const validate = async () => {
+    console.log(form.value);
     const { valid } = await form.value.validate()
 
-    if (valid) alert('Form is valid')
+    if (valid) saveNewFamilyMember()
+}
+const saveNewFamilyMember = async () => {
+    loading.value = true
+    UserService.create(userData.value)
+        .then((response) => {
+            console.log(response);
+        })
+        .catch((error) => {
+            console.error(error);
+        })
+        .finally(() => {
+            loading.value = false
+        })
 }
 function reset() {
     form.value.reset()
---
+++ services/BaseHttpRequester.js
@@ -0,0 +1,15 @@
+export class BaseHttpRequester {
+  static BASE_URL = "http://localhost:8199/api/v1/";
+  static getToken() {
+    return window.localStorage.getItem("ACCESS_TOKEN");
+  }
+  static getHeaders() {
+    const token = this.getToken();
+    return {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json", // Adjust content type as needed
+      },
+    };
+  }
+}
